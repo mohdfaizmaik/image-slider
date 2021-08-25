160b20b9cd90379148aa62389707f65ed638f52d
mohdfaizmaik <9696faiz@gmail.com>
2021-08-26 03:43:52 +0530

    functions are created for Controls to go to next/prev slide

--- src/Components/ImageSlider.js
+++ src/Components/ImageSlider.js
@@ -6,11 +6,18 @@ const ImageSlider = ({slides}) => {
     let [current, setCurrent] = useState(0);
     const length = slides.length;
 
+    const nextSlide=() => {
+        setCurrent(current === length-1?0 : current+1);
+    }
+
+    const previousSlide = () => {
+        setCurrent(current === 0?length-1 : current+1);
+    }
 
     return(
         <section className= "slider">
-            <FaArrowAltCircleLeft className="left" />
-            <FaArrowAltCircleRight className = "right" />
+            <FaArrowAltCircleLeft className="left" onClick = {previousSlide}/>
+            <FaArrowAltCircleRight className = "right" onClick ={nextSlide}/>
             {SliderData.map( (slide, index) => {
                 return <img src = {slide.image} alt ="lake pics" className="image"/>;
             })}
