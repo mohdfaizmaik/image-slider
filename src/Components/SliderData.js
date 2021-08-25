e6db57c9c14197777c3592884458c35c834621f0
mohdfaizmaik <9696faiz@gmail.com>
2021-08-26 02:45:04 +0530

    images are added to show in slider

---
+++ src/Components/SliderData.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const SliderData = [
+    {
+        image:'https://www.shutterstock.com/image-photo/sunrise-view-dal-lake-srinagar-jammu-654201766'
+    },
+    {
+        image:'https://www.shutterstock.com/image-photo/srinagar-jammu-kashmir-india-november-16-774669184'
+    },
+    {
+        image:'https://www.shutterstock.com/image-photo/starry-night-india-kashmir-dal-lake-1195450543'
+    },
+    {
+        Image:'https://www.shutterstock.com/image-photo/beauty-dal-lake-beautiful-shikaras-during-1189787239'
+    },
+    {
+        image:'https://www.shutterstock.com/image-photo/srinagar-jammu-kashmirindia-may-6-2019-1411322615'
+    }
+];
