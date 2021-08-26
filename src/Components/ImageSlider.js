83ab3bb7db6b86db60a3b1fd31a13a80f7eb2748
mohdfaizmaik <9696faiz@gmail.com>
2021-08-26 12:52:50 +0530

    functionality added to jump to a specific slide

--- src/Components/ImageSlider.js
+++ src/Components/ImageSlider.js
@@ -1,17 +1,22 @@
 import React,{useState} from 'react';
 import { SliderData } from './SliderData';
 import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa';
+import '../App';
 
 const ImageSlider = ({slides}) => {
-    let [current, setCurrent] = useState(0);
+    let [current, setCurrent] = useState(1);
     const length = slides.length;
 
     const nextSlide=() => {
-        setCurrent(current === length-1?0 : current+1);
+        setCurrent(current === length ?0 : current+1);
     }
 
     const previousSlide = () => {
-        setCurrent(current === 0?length-1 : current+1);
+        setCurrent(current === 1 ?length : current-1);
+    }
+
+    const moveDot = (index) => {
+        setCurrent(index)
     }
 
     return(
@@ -20,14 +25,21 @@ const ImageSlider = ({slides}) => {
             <FaArrowAltCircleRight className = "right" onClick ={nextSlide}/>
             {SliderData.map( (slide, index) => {
                 return (
-                    <div className = {index === current? 'slide-active' : 'slide'}
+                    <div className = {index+1 === current? 'slide-active' : 'slide'}
                     key = {index}>
-                      {index ===current &&(  <img src = {slide.image} alt ="lake pics" className="image"/>
+                      {index+1 ===current &&(  <img src = {slide.image} alt ="lake pics" className="image"/>
                         )}
                     </div>
-                );
-                
+                ); 
             })}
+             <div className="container-dots">
+                {Array.from({length}).map((slide, index) => (
+                    <div 
+                    onClick={() => moveDot(index + 1)}
+                    className={current === index + 1 ? "dot active" : "dot"}
+                    ></div>
+                ))}
+                </div>
 
         </section>
     )
